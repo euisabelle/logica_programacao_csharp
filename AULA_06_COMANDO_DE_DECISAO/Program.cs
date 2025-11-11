97cd1cb97fa3484814e5fda4c0715c16cbe6ddd2
Isabelle Melo <seuemail@exemplo.comisabellefernandamelo@hotmail.com>
2025-11-10 21:54:08 -0300

    decision command

---
+++ AULA_06_COMANDO_DE_DECISAO/Program.cs
@@ -0,0 +1,53 @@
+﻿//
+
+const int maioridade = 18;
+const int ZERO = 0;
+
+Console.WriteLine("Qual a sua idade? ");
+
+string resposta = Console.ReadLine();
+
+bool ehValido = int.TryParse(resposta, out int idade);
+
+if (ehValido)
+{
+    if (idade >= maioridade)
+    {
+        Console.WriteLine("Usuário é maior de idade");
+    }
+    else if (idade < ZERO)
+    {
+        Console.WriteLine("Idade Inválida");
+    }
+    else
+    {
+        Console.WriteLine("Usuário é menor de idade");
+    }
+}
+else
+{
+    Console.WriteLine("Valor Inválido");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
